644ecb0267ef5b833cd63ed76d47e9c516fbb567
James Blackwood-Sewell <james.sewell@gmail.com>
2026-04-23 16:35:47 +1200

    Fix Elastic to use TLS

--- backends/elasticsearch/register.go
+++ backends/elasticsearch/register.go
@@ -2,24 +2,46 @@
 package elasticsearch
 
 import (
+	"os"
+	"strconv"
+	"strings"
+
 	"github.com/paradedb/benchmarks/backends"
 	"github.com/paradedb/benchmarks/backends/shared/elastic"
 )
+
+const skipTLSVerifyEnv = "ELASTICSEARCH_SKIP_TLS_VERIFY"
 
 func init() {
 	backends.Register("elasticsearch", backends.BackendConfig{
 		Factory:     New,
 		FileType:    "json",
 		EnvVar:      "ELASTICSEARCH_URL",
-		DefaultConn: "http://localhost:9200",
+		DefaultConn: "https://elastic:elastic@localhost:9200",
 		Container:   "elasticsearch",
 	})
 }
 
+func driverConfig() elastic.DriverConfig {
+	return elastic.DriverConfig{
+		SkipTLSVerify:    envBool(skipTLSVerifyEnv, true),
+		VersionInfoField: "build_flavor",
+	}
+}
+
+func envBool(name string, defaultVal bool) bool {
+	value := strings.TrimSpace(os.Getenv(name))
+	if value == "" {
+		return defaultVal
+	}
+	enabled, err := strconv.ParseBool(value)
+	if err != nil {
+		return defaultVal
+	}
+	return enabled
+}
+
 // New creates a new Elasticsearch driver.
 func New(connString string) (backends.Driver, error) {
-	return elastic.New(connString, elastic.DriverConfig{
-		SkipTLSVerify:    false,
-		VersionInfoField: "build_flavor",
-	})
+	return elastic.New(connString, driverConfig())
 }
